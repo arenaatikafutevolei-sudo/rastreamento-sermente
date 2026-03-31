2acd58fd08512e1c79f42963ef3f612a3a59fc7d
arenaatikafutevolei-sudo <arenaatikafutevolei@gmail.com>
2026-03-31 18:50:56 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -90,4 +90,8 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
